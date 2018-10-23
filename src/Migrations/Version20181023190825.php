699b3a8949cb5ade7889229ede2da9a880c1eb35
Romain <bigzounet@hotmail.com>
2018-10-24 00:38:16 +0300

    feat(gigya-login): to be compatible with the way gigya store id, we need to store them as 300 characters strings

---
+++ src/Migrations/Version20181023190825.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * userId is now the Gigya's UID
+ */
+final class Version20181023190825 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     *
+     * @throws \Doctrine\DBAL\DBALException
+     * @throws \Doctrine\DBAL\Migrations\AbortMigrationException
+     */
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE card ALTER userid TYPE VARCHAR(300)');
+        $this->addSql('ALTER TABLE card ALTER userid DROP DEFAULT');
+    }
+
+    /**
+     * @param Schema $schema
+     *
+     * @throws \Doctrine\DBAL\DBALException
+     * @throws \Doctrine\DBAL\Migrations\AbortMigrationException
+     */
+    public function down(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE card ALTER userid TYPE INT');
+        $this->addSql('ALTER TABLE card ALTER userid DROP DEFAULT');
+        $this->addSql('ALTER TABLE card ALTER userid TYPE INT');
+    }
+}
